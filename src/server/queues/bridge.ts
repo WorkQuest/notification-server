edca6d6e8ad5000313d7be0cb845233501463c87
Ilya Kasimov <adamoffilya@mail.ru>
2022-02-16 18:39:55 +0700

    [update] fix bridge notification

--- src/server/queues/bridge.ts
+++ src/server/queues/bridge.ts
@@ -2,6 +2,6 @@ import { checkSuccessQueue } from '../utils/queues';
 import { publishBridgeNotifications } from '../websocket/websocket.bridge';
 
 export async function bridgeNotifications(context, message): Promise<void> {
-  await publishBridgeNotifications(context.data.recipient, context);
+  await publishBridgeNotifications(context.recipient, context);
   await checkSuccessQueue(message, false);
 }
