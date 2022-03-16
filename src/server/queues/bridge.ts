34f1d650586ba46d272d95293dc4c4238dc6720d
Ilya Kasimov <adamoffilya@mail.ru>
2022-03-16 17:48:51 +0700

    [update] Change bridge websocket recipient

--- src/server/queues/bridge.ts
+++ src/server/queues/bridge.ts
@@ -2,6 +2,6 @@ import { checkSuccessQueue } from '../utils/queues';
 import { publishBridgeNotifications } from '../websocket/websocket.bridge';
 
 export async function bridgeNotifications(context, message): Promise<void> {
-  await publishBridgeNotifications(context.recipient, context);
+  await publishBridgeNotifications(context.recipients[0], context);
   await checkSuccessQueue(message, false);
 }
